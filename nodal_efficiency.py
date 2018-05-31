2a16d1b7ed0fb24e88953cdf63c42996fce372f3
Serafeim Loukas <25902670+seralouk@users.noreply.github.com>
2018-05-31 14:14:08 +0200

    Update nodal_efficiency.py

--- nodal_efficiency.py
+++ nodal_efficiency.py
@@ -1,13 +1,19 @@
 from numpy import *
 from igraph import *
 
-
-g=Graph.Erdos_Renyi(100,0.7)
-g.es['weight'] = random.random(g.ecount())
-print("The number of nodes is:{}".format(g.vcount()))
-
 def nodaleff(g):
-
+    """
+    This function returns the nodal efficiency of all nodes of a graph object.
+    
+    Input
+    ------
+    g: an iGraph graph object
+    
+    Output
+    ne: nodal efficiency of all the nodes
+    ------
+    
+    """
     g.es["weight"] = [1.0 / x for x in g.es["weight"]]
     sp = g.shortest_paths_dijkstra(weights=g.es["weight"])
     sp = asarray(sp)
@@ -18,4 +24,8 @@ def nodaleff(g):
     return ne
 
 if __name__ =="__main__":
+    
+    g=Graph.Erdos_Renyi(100,0.7)
+    g.es['weight'] = random.random(g.ecount())
+    print("The number of nodes is:{}".format(g.vcount()))
     print("the nodal eff of each node is: {}".format(nodaleff(g)))
